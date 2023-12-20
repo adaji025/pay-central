6599e87addfc73a92a5f954c2edf8539108c0b74
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-12-20 21:55:40 +0100

    added the footer and cookies sections

---
+++ app/components/FreeProviders.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CompanyCard from "./CompanyCard";
+import { IoChevronForwardOutline } from "react-icons/io5";
+
+const FreeProviders = () => {
+  return (
+    <div className="my-10">
+      <div className="flex justify-between mb-5">
+        <div className="text-lg sm:text-2xl font-semibold">
+          Free Curated Providers
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="text-sm">Explore methods curated for you</div>
+          <IoChevronForwardOutline />
+        </div>
+      </div>
+      <div className="grid gap-5">
+        {[...Array(2)].map((_, i) => (
+          <CompanyCard />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FreeProviders;
---
+++ app/components/PaymentProviders.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { Menu } from "@mantine/core";
+import { IoChevronDown } from "react-icons/io5";
+import CompanyCard from "./CompanyCard";
+
+const PaymentProviders = () => {
+  return (
+    <div className="my-10">
+      <div className="flex justify-between mb-5">
+        <div className="text-lg sm:text-2xl font-semibold">
+          Top payment providers
+        </div>
+        <Menu position="left-start" shadow="md" width={200}>
+          <Menu.Target>
+            <button className="bg-gray-100 flex gap-2 items-center px-5 py-2">
+              <div>Featured</div>
+              <IoChevronDown />
+            </button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item>one</Menu.Item>
+            <Menu.Item>one</Menu.Item>
+            <Menu.Item>one</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </div>
+      <div className="grid gap-5">
+              {[...Array(4)].map((_, i) => (
+                <CompanyCard />
+              ))}
+            </div>
+    </div>
+  );
+};
+
+export default PaymentProviders;
